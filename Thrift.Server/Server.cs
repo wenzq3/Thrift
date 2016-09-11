46e72659d50f6f483c0ce2e2572946b05151aefc
温 <ig.start@qq.com>
2016-09-11 15:15:25 +0800

    no message

--- Thrift.Server/Server.cs
+++ Thrift.Server/Server.cs
@@ -16,7 +16,7 @@ namespace Thrift.Server
     public class Server
     {
         private static Dictionary<TServer, RegeditConfig> _services;
-        private const int _delayedTime = 5000;//延时关闭服务时间
+        private const int _delayedTime = 20000;//延时关闭服务时间
 
         public static void Start()
         {
